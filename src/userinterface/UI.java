a3dfb09f22527eea168f59906acefa0516ab322f
AR-C05 <62663359+AR-C05@users.noreply.github.com>
2022-05-30 03:19:15 -0400

    starting to add functionality

--- src/userinterface/UI.java
+++ src/userinterface/UI.java
@@ -1,8 +1,29 @@
 package userinterface;
 
+import data.CricketTeam;
+import data.Name;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class UI {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO finsish
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        CricketTeam home, away;
+        String tempString = "";
+        System.out.print("Enter the name of the home team: ");
+        try {
+            tempString = input.readLine().strip();
+        } catch (IOException e) {
+            System.err.println("An error occured with the input");
+            e.printStackTrace();
+        }
+        home = new CricketTeam(tempString);
+    }
+
+    public void getName() {
+        //
     }
 }
